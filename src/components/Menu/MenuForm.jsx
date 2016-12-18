9c36152cead6368251d7974d648e94010d5bebbf
MarsZhu <marno15@gmail.com>
2016-12-18 23:16:07 +0800

    for merge

--- src/components/Menu/MenuForm.jsx
+++ src/components/Menu/MenuForm.jsx
@@ -5,9 +5,20 @@ const FormItem=Form.Item;
 const MenuForm=( )=>{
 
     return (
-      <Form inline onSubmit={}>
-
+      <Form  onSubmit={}>
+        <FormItem>
+          {
+            getFieldDecorator ('userName',{
+              rules:[{
+                required:true,message:'请输入用户名'
+              }],
+            })(
+              <Input addonBefore={<Icon type="user"/>} placeholder="UserName"></Input>
+            )
+          }
+        </FormItem>
       </Form>
 
     );
 }
+export default MenuForm;
